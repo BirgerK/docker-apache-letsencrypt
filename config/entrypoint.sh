a4652e9a34b2fe03fb638a7fbd1b5250ff79f538
BirgerK <birger.kamp@iteratec.de>
2016-08-08 12:42:54 +0200

    run init-letsencrypt only if DOMAIN is set

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# init only if lets-encrypt is running for the first time
-([ ! -d $LETSENCRYPT_HOME ] || [ ! "$(ls -A $LETSENCRYPT_HOME)" ]) && /init_letsencrypt.sh --domains $DOMAINS && service apache2 stop
+# init only if lets-encrypt is running for the first time and if DOMAINS was set
+([ ! -d $LETSENCRYPT_HOME ] || [ ! "$(ls -A $LETSENCRYPT_HOME)" ]) && [ ! -z "$DOMAINS" ] && /init_letsencrypt.sh --domains $DOMAINS && service apache2 stop
 
 # run supervisor, which runs apache and cron
 /usr/bin/supervisord
