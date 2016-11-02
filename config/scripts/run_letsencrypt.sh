08df81acb0feef4db3cf530d6d1ea3ca96d53592
Marco Vettorello <vettorello.marco@gmail.com>
2016-11-02 17:17:44 +0100

    Adding staging environment

--- config/scripts/run_letsencrypt.sh
+++ config/scripts/run_letsencrypt.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-letsencrypt -n --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+if (! [ -z "$STAGING" ]) then
+  echo "Using Let's Encrypt Staging environment..."
+  letsencrypt -n --staging --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+else
+  echo "Using Let's Encrypt Productioun environment..."
+  letsencrypt -n --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
+fi
