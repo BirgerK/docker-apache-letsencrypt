4a5c943052449beb95e22cd8e3365df47fddce62
BirgerK <birger.kamp@iteratec.de>
2016-12-01 18:41:08 +0100

    fix fail2ban

--- config/scripts/run_fail2ban.sh
+++ config/scripts/run_fail2ban.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-exec fail2ban-server -f
+function setTimeZone {
+    if [ -f "/etc/timezone.host" ]; then
+        CLIENT_TIMEZONE=$(cat /etc/timezone)
+        HOST_TIMEZONE=$(cat /etc/timezone.host)
+
+        if [ "${CLIENT_TIMEZONE}" != "${HOST_TIMEZONE}" ]; then
+            echo "Reconfigure timezone to "${HOST_TIMEZONE}
+            echo ${HOST_TIMEZONE} > /etc/timezone
+            dpkg-reconfigure -f noninteractive tzdata
+        fi
+    fi
+}
+
+setTimeZone
+fail2ban-server -f -p /var/run/fail2ban/fail2ban.pid
