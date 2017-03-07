8a626e50ce12bdec916df19e8e80969499dce871
BirgerK <birger.kamp@iteratec.de>
2017-03-07 21:09:31 +0100

    cleanup apache pid-file on startup for a clean startup

--- config/scripts/run_apache.sh
+++ config/scripts/run_apache.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 source /etc/apache2/envvars
+
+if [ -f /var/run/apache2/apache2.pid ]; then
+    apachePid=$(cat /var/run/apache2/apache2.pid)
+    echo "Found file 'apache2.pid' with pid $apachePid. Let's check if there is a process belonging to it!"
+    processName=$(ps -p $apachePid -o comm)
+
+    if [ "$processName" != "apache2" && "$processName" != "/usr/sbin/apache2 -D FOREGROUND" ]; then
+        echo "The found apache.pid is not belonging to an apache-process. I'm going to remove the pid-file."
+        rm -f /var/run/apache2/apache2.pid
+        echo "Removing of pid-file was successfull."
+
+        echo "For cleaning-reasons I'm also killing all apache-processes."
+        killall -9 apache2
+        echo "Now this is a safe place again. Enjoy."
+      else
+        echo "The pid-file is belonging to an apache-process, so it will stay alive."
+    fi
+fi
+
 exec /usr/sbin/apache2 -D FOREGROUND
