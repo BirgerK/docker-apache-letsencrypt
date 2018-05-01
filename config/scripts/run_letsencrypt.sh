2bbba91a7b1e58a3ac53639ba3198ec83a5688c2
BirgerK <birger.kamp@iteratec.de>
2018-05-01 15:19:47 +0200

    fix log message

--- config/scripts/run_letsencrypt.sh
+++ config/scripts/run_letsencrypt.sh
@@ -3,6 +3,6 @@ if (! [ -z "$STAGING" ]) then
   echo "Using Let's Encrypt Staging environment..."
   letsencrypt -n --staging --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
 else
-  echo "Using Let's Encrypt Productioun environment..."
+  echo "Using Let's Encrypt Production environment..."
   letsencrypt -n --expand --apache --agree-tos --email $WEBMASTER_MAIL "$@"
 fi
